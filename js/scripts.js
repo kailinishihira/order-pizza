2d843b2892d7e05c6bc2b8df243693ea00c9a62e
Kaili Nishihira <kailinishihira@gmail.com>
2017-07-21 10:00:23 -0700

    Change forms to drop down

--- js/scripts.js
+++ js/scripts.js
@@ -12,33 +12,15 @@ function Pizza (size, toppings, additionalToppings) {
 //UI logic:
 $(document).ready(function() {
   $("#more-toppings").click(function() {
-    $("#add-toppings").append('<div class="add-toppings">' +
-                                '<div class="radio">' +
-                                  '<label>' +
-                                    '<input type="radio" name="toppings" value="pepperoni">Pepperoni' +
-                                  '</label>' +
-                                '</div>' +
-                                '<div class="radio">' +
-                                  '<label>' +
-                                    '<input type="radio" name="toppings" value="mushrooms">Mushrooms' +
-                                  '</label>' +
-                                '</div>' +
-                                '<div class="radio">' +
-                                  '<label>' +
-                                    '<input type="radio" name="toppings" value="bacon">Bacon'+
-                                  '</label>' +
-                                '</div>' +
-                                '<div class="radio">' +
-                                  '<label>' +
-                                    '<input type="radio" name="toppings" value="anchovies">Anchovies' +
-                                  '</label>' +
-                                '</div>' +
-                                '<div class="radio">' +
-                                  '<label>' +
-                                    '<input type="radio" name="toppings" value="extra-cheese">Extra-cheese' +
-                                  '</label>' +
-                                '</div>' +
-                              '</div>');
+    $("#add-toppings").append('<h3>Additional Toppings</h3>' +
+                                '<div class="add-toppings">' +
+                                  '<select class="form-control" id="toppings">' +
+                                    '<option value="1">Pepperoni</option>' +
+                                    '<option value="2">Mushrooms</option>' +
+                                    '<option value="3">Bacon</option>' +
+                                    '<option value="4">Anchovies</option>' +
+                                    '<option value="5">Extra Cheese</option>' +
+                                  '</select>');
   });
 
   $("form#pizza-order").submit(function(event) {
@@ -49,11 +31,36 @@ $(document).ready(function() {
     var inputPizzaToppings = $(this).find("input:radio[name=toppings]:checked").val();
     var newPizza = new Pizza(inputPizzaSize, inputPizzaToppings);
     newPizza.toppings.push(inputPizzaToppings);
-    });
-
-
-
+    // });
 
   });
-
 });
+
+
+// '<div class="add-toppings">' +
+//   '<div class="radio">' +
+//     '<label>' +
+//       '<input type="radio" name="toppings" value="pepperoni">Pepperoni' +
+//     '</label>' +
+//   '</div>' +
+//   '<div class="radio">' +
+//     '<label>' +
+//       '<input type="radio" name="toppings" value="mushrooms">Mushrooms' +
+//     '</label>' +
+//   '</div>' +
+//   '<div class="radio">' +
+//     '<label>' +
+//       '<input type="radio" name="toppings" value="bacon">Bacon'+
+//     '</label>' +
+//   '</div>' +
+//   '<div class="radio">' +
+//     '<label>' +
+//       '<input type="radio" name="toppings" value="anchovies">Anchovies' +
+//     '</label>' +
+//   '</div>' +
+//   '<div class="radio">' +
+//     '<label>' +
+//       '<input type="radio" name="toppings" value="extra-cheese">Extra-cheese' +
+//     '</label>' +
+//   '</div>' +
+// '</div>');
