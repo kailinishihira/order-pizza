e7e11863e505cd3f007de5255cc1ed6a071d778d
Kaili Nishihira <kailinishihira@gmail.com>
2017-07-21 10:44:59 -0700

    Changes values of forms

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function Pizza (size, toppings, additionalToppings) {
 // Pizza.prototype.cost = function() {
 //   return this.size + this.toppings;
 // }
-
+var newPizza;
 
 //UI logic:
 $(document).ready(function() {
@@ -15,25 +15,34 @@ $(document).ready(function() {
     $("#add-toppings").append('<h3>Additional Toppings</h3>' +
                                 '<div class="add-toppings">' +
                                   '<select class="form-control" id="toppings">' +
-                                    '<option value="1">Pepperoni</option>' +
-                                    '<option value="2">Mushrooms</option>' +
-                                    '<option value="3">Bacon</option>' +
-                                    '<option value="4">Anchovies</option>' +
-                                    '<option value="5">Extra Cheese</option>' +
+                                    '<option value="Pepperoni">Pepperoni</option>' +
+                                    '<option value="Mushrooms">Mushrooms</option>' +
+                                    '<option value="Bacon">Bacon</option>' +
+                                    '<option value="Anchovies">Anchovies</option>' +
+                                    '<option value="Extra Cheese">Extra Cheese</option>' +
                                   '</select>');
   });
 
   $("form#pizza-order").submit(function(event) {
+    debugger;
     event.preventDefault();
-
     var inputPizzaSize = $("#pizza-size").val();
-    // $(".add-toppings").each(function() {
-    var inputPizzaToppings = $(this).find("#toppings").val();
-    var newPizza = new Pizza(inputPizzaSize, inputPizzaToppings);
-    newPizza.toppings.push(inputPizzaToppings);
-    // });
+  //   // $(".add-toppings").each(function() {
+    var inputPizzaToppings = $(this).find(".add-toppings").val();
+    var allToppings = [];
+    allToppings.push(inputPizzaToppings);
 
+    $(".size-ordered").text(inputPizzaSize);
+    $(".toppings-ordered").text(allToppings);
+  //   newPizza = new Pizza(inputPizzaSize, inputPizzaToppings);
+  //   newPizza.toppings.push(inputPizzaToppings);
+  //   // });
   });
+  //
+  // $(".size-ordered").text(newPizza.size());
+  //   newPizza.toppings.forEach(function() {
+  //   $(".toppings-ordered").append("<li>" + newPizza.toppings + "</li>");
+  // });
 });
 
 
